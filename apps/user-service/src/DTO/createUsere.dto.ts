1272a247f569b199d203d0ef83bc8cd27918f5e3
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-07-21 18:50:21 +0530

    update dto for the user signup

--- apps/user-service/src/DTO/createUsere.dto.ts
+++ apps/user-service/src/DTO/createUsere.dto.ts
@@ -1,4 +1,12 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -6,4 +14,19 @@ export class CreateUserDto {
   @MinLength(2)
   @MaxLength(20)
   name: string;
+
+  @IsNumber()
+  @Length(10)
+  @IsNotEmpty()
+  phonrNumber: number;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @IsNotEmpty()
+  password: string;
 }
