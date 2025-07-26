1a6478f5b741d958ba460a15526e68ca17f22cd5
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-07-26 23:39:02 +0530

    this commit is setup the jwt strategy for the genrate the jwt token and verify and protect the jwt token for protect the route

---
+++ apps/user-service/src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() body: any) {
+    const user = await this.authService.validateUser(
+      body.username,
+      body.password,
+    );
+    if (!user) return { message: 'Invalid credentials' };
+    return this.authService.login(user);
+  }
+}
