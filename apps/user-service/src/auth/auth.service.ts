ef02bfa93729efca28099032273a97746071faa0
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-07-26 22:54:47 +0530

    this is normal commit

--- apps/user-service/src/auth/auth.service.ts
+++ apps/user-service/src/auth/auth.service.ts
@@ -1,2 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.findByUsername(username);
+    if (user && user.password === password) {
+      const { password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+}
